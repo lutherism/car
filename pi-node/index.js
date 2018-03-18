a0dd1e76b39daa0cfd3bea662da18a8de35f50ec
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2018-03-18 00:28:20 -0700

    gpio

--- pi-node/index.js
+++ pi-node/index.js
@@ -1,18 +1,40 @@
 var gpio = require("gpio");
+var Gpio = require('pigpio').Gpio,
+  motor = new Gpio(10, {mode: Gpio.OUTPUT}),
+  pulseWidth = 1000,
+  increment = 100;
 
-// Calling export with a pin number will export that header and return a gpio header instance
-var gpio4 = gpio.export(4, {
-   // When you export a pin, the default direction is out. This allows you to set
-   // the pin value to either LOW or HIGH (3.3V) from your program.
-   direction: 'out',
+const MOTORS = {
+  '1a': 11,
+  '1b': 12,
+  '2a': 13,
+  '2b': 15
+};
 
-   // set the time interval (ms) between each read when watching for value changes
-   // note: this is default to 100, setting value too low will cause high CPU usage
-   interval: 200,
+const PINS = {
+  '1': 4,
+  '2': 5
+};
 
-   // Due to the asynchronous nature of exporting a header, you may not be able to
-   // read or write to the header right away. Place your logic in this ready
-   // function to guarantee everything will get fired properly
-   ready: function() {
-   }
+Promise.all(Object.keys(MOTORS).map(motorKey => {
+  return new Promise((reject, resolve) => {
+    const motor = gpio.export(MOTORS[motorKey], {
+       // When you export a pin, the default direction is out. This allows you to set
+       // the pin value to either LOW or HIGH (3.3V) from your program.
+       direction: 'out',
+
+       // set the time interval (ms) between each read when watching for value changes
+       // note: this is default to 100, setting value too low will cause high CPU usage
+       interval: 200,
+
+       // Due to the asynchronous nature of exporting a header, you may not be able to
+       // read or write to the header right away. Place your logic in this ready
+       // function to guarantee everything will get fired properly
+       ready: function() {
+         resolve(motor);
+       }
+     });
+   });
+})).then(motors => {
+  motors.map(motor => motor.set());
 });
