2279a563391aa042b69598fbaffaf0e5e46b412d
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2018-03-18 00:07:56 -0700

    gpio

--- pi-node/index.js
+++ pi-node/index.js
@@ -1,7 +1,18 @@
-var gpio = require("pi-gpio");
+var gpio = require("gpio");
 
-gpio.open(16, "output", function(err) {		// Open pin 16 for output
-    gpio.write(16, 1, function() {			// Set pin 16 high (1)
-        gpio.close(16);						// Close pin 16
-    });
+// Calling export with a pin number will export that header and return a gpio header instance
+var gpio4 = gpio.export(4, {
+   // When you export a pin, the default direction is out. This allows you to set
+   // the pin value to either LOW or HIGH (3.3V) from your program.
+   direction: 'out',
+
+   // set the time interval (ms) between each read when watching for value changes
+   // note: this is default to 100, setting value too low will cause high CPU usage
+   interval: 200,
+
+   // Due to the asynchronous nature of exporting a header, you may not be able to
+   // read or write to the header right away. Place your logic in this ready
+   // function to guarantee everything will get fired properly
+   ready: function() {
+   }
 });
