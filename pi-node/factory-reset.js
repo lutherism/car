6295ccd6968d34772bbc1bc9817bcb2c04061b56
Alexander Jansen <alexanderjansen@alexanders-mbp.lan>
2020-11-02 22:21:04 -0800

    freset

--- pi-node/factory-reset.js
+++ pi-node/factory-reset.js
@@ -7,10 +7,9 @@ const DEFAULT_DEVICE_UUID = '6be50aff-6f10-4643-bfda-7d5bf15319c9';
 fs.readFile('./openroboticsdata/data_template.json', data => {
   const newDriverUuid = uuid();
   fs.writeFile('./openroboticsdata/data.json', JSON.stringify({
-    deviceUuid: newDriverUuid,
-    ...data
+    deviceUuid: newDriverUuid
   }), () => {});
-  request.post('http://localhost:3001/api/device', {
+  request.post('https://robots-gateway.uc.r.appspot.com/api/device', {
     json: true,
     body: {
       uuid: newDriverUuid,
