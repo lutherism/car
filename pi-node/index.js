ec0ff27026aea7dfca9c804cbf45d67e91ca2b28
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2020-11-04 11:12:32 -0800

    mtoor c

--- pi-node/index.js
+++ pi-node/index.js
@@ -9,82 +9,27 @@ var options = {
     frequency: 60,
     debug: true
 };
-const s = {
 
-};
+const COIL_PINS = [
+  17,
+  18,
+  27,
+  22
+];
+const orders = [
+  [0, 1, 2, 3]
+];
 
-const pwm = new Pca9685Driver(options, function(err) {
-    if (err) {
-        console.error("Error initializing PCA9685", err);
-        process.exit(-1);
-    }
-    console.log("Initialization done");
-    const steeringMid = 1500;
-    const speedMid = 200;
-
-    // Set the pulse length to 1500 microseconds for channel 2
-    pwm.setPulseLength(0, steeringMid);
-    pwm.setPulseLength(4, speedMid);
-    pwm.setPulseLength(5, speedMid);
-    var next = -150;
-
-    // Turn off all power to channel 6
-    // (with optional callback)
-    pwm.channelOn(4);
-    pwm.channelOn(5);
-    pwm.channelOn(0);
-    s.pwm = pwm;
-    s.h = () => {
-      pwm.setPulseLength(0, 1500);
-    }
-    s.l = () => {
-      pwm.setPulseLength(0, 1350);
-    }
-    s.r = () => {
-      pwm.setPulseLength(0, 1650);
-    }
-    s.sp = (speed) => {
-      pwm.setPulseLength(4, speed);
-      pwm.setPulseLength(5, speed);
-    }
-    s.f = () => {
-      s.motors[0].set(0);
-      s.motors[1].set();
-      s.motors[2].set(0);
-      s.motors[3].set();
-    }
-    s.s = () => {
-      s.motors[0].set(0);
-      s.motors[1].set(0);
-      s.motors[2].set(0);
-      s.motors[3].set(0);
-    }
-    s.b = () => {
-      s.motors[0].set();
-      s.motors[1].set(0);
-      s.motors[2].set();
-      s.motors[3].set(0);
-    }
-});
-
-
-const MOTORS = {
-  '1a': 17,
-  '1b': 18,
-  '2a': 27,
-  '2b': 22
-};
-
-Promise.all(Object.keys(MOTORS).map(motorKey => {
+Promise.all(Object.keys(COIL_PINS).map(motorKey => {
   return new Promise((resolve, reject) => {
-    const motor = gpio.export(MOTORS[motorKey], {
+    const motor = gpio.export(COIL_PINS[motorKey], {
        // When you export a pin, the default direction is out. This allows you to set
        // the pin value to either LOW or HIGH (3.3V) from your program.
        direction: 'out',
 
        // set the time interval (ms) between each read when watching for value changes
        // note: this is default to 100, setting value too low will cause high CPU usage
-       interval: 400,
+       interval: 100,
 
        // Due to the asynchronous nature of exporting a header, you may not be able to
        // read or write to the header right away. Place your logic in this ready
@@ -95,13 +40,17 @@ Promise.all(Object.keys(MOTORS).map(motorKey => {
      });
    });
 })).then(motors => {
-  var f = true;
-  motors[0].set(0);
-  motors[1].set(0);
-  motors[2].set(0);
-  motors[3].set(0);
-  console.log('setting motors', motors.map(m => m.value));
-  s.motors = motors;
+  let order = 0;
+  let ActiveCoil = 0;
+  setInterval(() => {
+    const orderMappedCoilI = orders[order][ActiveCoil]
+    motors.map((m, i) => {
+      m.set(orderMappedCoilI === i ? 1 : 0)
+    });
+    ActiveCoil = (ActiveCoil + 1) % COIL_INS.length;
+    order = (order + 1) % orders.length;
+    console.log(motors.map(m => m.value));
+  }, 50);
 });
 
 
