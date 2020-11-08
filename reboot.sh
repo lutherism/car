3452e14dbc54d4889b8b74ac1a23d36f00a06d2d
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2020-11-07 21:57:26 -0800

    keep-alive

--- reboot.sh
+++ reboot.sh
@@ -3,3 +3,19 @@
 BASEDIR=$(dirname "$0")
 ~/.nvm/nvm.sh use 8
 node $BASEDIR/pi-node/keep-alive.js
+
+#!/bin/bash
+
+export DISPLAY=:0 #needed if you are running a simple gui app.
+
+process=node
+makerun="node $BASEDIR/pi-node/keep-alive.js"
+
+if ps ax | grep -v grep | grep $process > /dev/null
+then
+    exit
+else
+    $makerun &
+fi
+
+exit
