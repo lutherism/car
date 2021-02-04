2505666c7295fdeacb662260cffae30ab0147ec3
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2021-02-03 17:28:36 -0800

    reset

--- pi-node/factory-reset.js
+++ pi-node/factory-reset.js
@@ -6,9 +6,10 @@ const DEFAULT_DEVICE_UUID = '6be50aff-6f10-4643-bfda-7d5bf15319c9';
 
 fs.readFile('./openroboticsdata/data_template.json', data => {
   const newDriverUuid = uuid();
-  fs.writeFile('./openroboticsdata/data.json', JSON.stringify({
+  fs.unlinkSync(__dirname + '/openroboticsdata/data.json');
+  fs.writeFileSync(__dirname + '/openroboticsdata/data.json', JSON.stringify({
     deviceUuid: newDriverUuid
-  }), () => {});
+  }));
   request.post('https://robots-gateway.uc.r.appspot.com/api/device', {
     json: true,
     body: {
