3abda915fd5d5cedb98c572e6d0d051a51d7eb8e
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-06-18 15:57:58 -0700

    work

--- pi-node/commands.js
+++ pi-node/commands.js
@@ -24,29 +24,6 @@ let motorsContext = [];
 let order = 0;
 let ActiveCoil = 0;
 
-Promise.all(Object.keys(COIL_PINS).map(motorKey => {
-  return new Promise((resolve, reject) => {
-    const motor = gpio.export(COIL_PINS[motorKey], {
-       // When you export a pin, the default direction is out. This allows you to set
-       // the pin value to either LOW or HIGH (3.3V) from your program.
-       direction: 'out',
-
-       // set the time interval (ms) between each read when watching for value changes
-       // note: this is default to 100, setting value too low will cause high CPU usage
-       interval: 100,
-
-       // Due to the asynchronous nature of exporting a header, you may not be able to
-       // read or write to the header right away. Place your logic in this ready
-       // function to guarantee everything will get fired properly
-       ready: function() {
-         resolve(motor);
-       }
-     });
-   });
-})).then(motors => {
-  motorsContext = motors;
-});
-
 const COMMANDS = {
   'update': () => {
     const st = spawn('cd /home/pi/projects/car && sudo git pull && sudo reboot');
@@ -133,8 +110,33 @@ const COMMANDS = {
          }
        });
      });
+  },
+  export: () => {
+    Promise.all(Object.keys(COIL_PINS).map(motorKey => {
+      return new Promise((resolve, reject) => {
+        const motor = gpio.export(COIL_PINS[motorKey], {
+           // When you export a pin, the default direction is out. This allows you to set
+           // the pin value to either LOW or HIGH (3.3V) from your program.
+           direction: 'out',
 
+           // set the time interval (ms) between each read when watching for value changes
+           // note: this is default to 100, setting value too low will cause high CPU usage
+           interval: 100,
+
+           // Due to the asynchronous nature of exporting a header, you may not be able to
+           // read or write to the header right away. Place your logic in this ready
+           // function to guarantee everything will get fired properly
+           ready: function() {
+             resolve(motor);
+           }
+         });
+       });
+    })).then(motors => {
+      motorsContext = motors;
+    });
   }
 }
+
+COMMANDS.export();
 
 module.exports = COMMANDS;
