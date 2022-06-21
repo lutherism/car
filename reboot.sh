7ade0018e7aff83063c4b6a1849d5b292c56c928
Alexander Jansen <alexanderjansen@Alexanders-MacBook-Pro.local>
2022-06-20 21:28:31 -0700

    work

--- reboot.sh
+++ reboot.sh
@@ -2,6 +2,7 @@
 
 BASEDIR="/home/pi/car"
 nvm use 8
+NODE_BIN="/home/pi/.config/nvm/versions/node/v8.17.0/bin/node"
 
 #!/bin/bash
 mkdir tmp
@@ -11,8 +12,8 @@ touch tmp/run-err.log
 echo $(date) Run >> $BASEDIR/tmp/reboot.log
 export DISPLAY=:0 #needed if you are running a simple gui app.
 
-process="node $BASEDIR/pi-node/keep-alive.js"
-makerun="node $BASEDIR/pi-node/keep-alive.js >> $BASEDIR/tmp/run.log 2>> $BASEDIR/tmp/run-err.log"
+process="$NODE_BIN $BASEDIR/pi-node/keep-alive.js"
+makerun="$NODE_BIN $BASEDIR/pi-node/keep-alive.js >> $BASEDIR/tmp/run.log 2>> $BASEDIR/tmp/run-err.log"
 
 echo Running $makerun
 
